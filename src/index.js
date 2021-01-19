dc2072b8c849e39eb0145091f66b23fe9c0aaa52
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-01-19 15:12:04 +0545

    fourth commits

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Cards from './Cards';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+  <div className="heading">
+    <h2>Download YTS YIFY movies: HD smallest size</h2>
+  </div>
+  <div className="mycard">
+  <Cards imgsrc="https://img.yts.mx/assets/images/movies/outside_the_wire_2021/medium-cover.jpg" title="Movies" sname="Out of the Wire" link="https://www.netflix.com/np/title/81074110" />
+  <Cards imgsrc="https://img.yts.mx/assets/images/movies/news_of_the_world_2020/medium-cover.jpg" title="Movies" sname="News of the world" link="https://yts.mx/movies/news-of-the-world-2020" />
+  <Cards imgsrc="https://img.yts.mx/assets/images/movies/locked_down_2021/medium-cover.jpg" title="Movies" sname="Locked Down" link="https://yts.mx/movies/locked-down-2021" />
+  <Cards imgsrc="https://img.yts.mx/assets/images/movies/wonder_woman_1984_2020/medium-cover.jpg" title="Movies" sname="Wonder Women" link="https://yts.mx/movies/wonder-woman-1984-2020" />
+  <Cards imgsrc="https://img.yts.mx/assets/images/movies/safe_inside_2019/medium-cover.jpg" title="Movies" sname="Safe Inside" link="https://yts.mx/movies/safe-inside-2019" />
+  <Cards imgsrc="https://img.yts.mx/assets/images/movies/twiceborn_2020/medium-cover.jpg" title="Movies" sname="Twiceborn" link="https://yts.mx/movies/twiceborn-2020" />
+  </div>
+  </>
+
+,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
